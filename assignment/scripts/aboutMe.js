5f87cf2dd16479fb260fc1693c37171615e49408
Jenny Lagervall <jennylagervall@gmail.com>
2024-07-27 13:52:56 -0700

    added name in aboutMe.Js

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,11 +1,15 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Jenny';
+console.log ('My first name is: ' , firstName)
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Lagervall'
+console.log ('my last name is:', lastName)
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
+let fullName = firstName + " " + lastName
+console.log ('My full name is:' , fullName)
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
